09384fab414370e9a104d85a05c113dd55d9ac35
Nillerr <800751+Nillerr@users.noreply.github.com>
2021-11-15 08:31:59 +0100

    Run code generator in CI

--- mockative-code-generator/src/main/kotlin/io/mockative/generator/Main.kt
+++ mockative-code-generator/src/main/kotlin/io/mockative/generator/Main.kt
@@ -3,7 +3,7 @@ package io.mockative.generator
 import java.nio.file.Paths
 
 fun main(args: Array<String>) {
-    val outputDir = args.getOrNull(0) ?: Paths.get("mockative/build/generated/mockative-code-generator").toAbsolutePath().toString()
+    val outputDir = args.getOrNull(0) ?: Paths.get("..", "mockative/build/generated/mockative-code-generator").toAbsolutePath().toString()
     println("Generating code in $outputDir")
 
     val generator = CodeGenerator(outputDir)
