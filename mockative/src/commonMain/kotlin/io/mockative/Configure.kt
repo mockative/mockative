0eb394d2fd785fce9abff1f8e0aa055941326905
Nillerr <800751+Nillerr@users.noreply.github.com>
2021-11-25 10:44:16 +0100

    Makes configure API experimental

--- mockative/src/commonMain/kotlin/io/mockative/Configure.kt
+++ mockative/src/commonMain/kotlin/io/mockative/Configure.kt
@@ -1,9 +1,16 @@
 package io.mockative
 
+@Retention(AnnotationRetention.BINARY)
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
+@RequiresOptIn(message = "This API is experimental. It may be changed in the future without notice.", level = RequiresOptIn.Level.WARNING)
+annotation class ConfigurationApi
+
+@ConfigurationApi
 fun <T : Any> configure(subject: T, block: MockConfiguration.() -> Unit): T {
     return subject.apply { block(MockConfiguration(asMockable())) }
 }
 
+@ConfigurationApi
 class MockConfiguration(private val mock: Mockable) {
     var stubsUnitByDefault: Boolean
         get() = mock.stubsUnitsByDefault
