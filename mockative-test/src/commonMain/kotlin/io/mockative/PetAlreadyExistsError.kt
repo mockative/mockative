0b84e864fab9ed573e1ddeb10807a4595eced4cd
Nicklas Jensen <800751+Nillerr@users.noreply.github.com>
2022-05-14 10:54:43 +0200

    Prepare release of 1.2.0

--- mockative-test/src/commonMain/kotlin/io/mockative/PetAlreadyExistsError.kt
+++
@@ -1,3 +0,0 @@
-package io.mockative
-
-data class PetAlreadyExistsError(val pet: Pet) : Error()
--- mockative-test/src/commonTest/resources/io/mockative/GeneratedMocks.kt
+++
@@ -1,4 +0,0 @@
-package io.mockative
-
-internal fun <T> mock(@Suppress("UNUSED_PARAMETER") type: kotlin.reflect.KClass<io.mockative.PetStore<T>>): io.mockative.PetStore<T> where T : kotlin.CharSequence = io.mockative.PetStoreMock<T>()
-internal fun mock(@Suppress("UNUSED_PARAMETER") type: kotlin.reflect.KClass<io.mockative.NoiseStore>): io.mockative.NoiseStore = io.mockative.NoiseStoreMock()
