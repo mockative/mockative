1ae1554b2eabefbc331b506b10c92c4de4a4f536
Nicklas Jensen <800751+Nillerr@users.noreply.github.com>
2023-08-16 22:25:33 +0200

    Support JDK 8

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -11,7 +11,7 @@ version = "1.0.0"
 group = "io.mockative"
 
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(8)
 
     js(IR) {
         browser()
@@ -103,8 +103,8 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 
     dependencies {
