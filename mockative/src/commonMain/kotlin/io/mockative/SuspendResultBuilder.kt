4ff034973848012ab227538f7c3146178b974605
Alexandre Favre <alexandre.favre@migrosonline.ch>
2024-04-19 11:09:28 +0200

    Allow infix notation on `invokes`, `returns` and `throws`

--- mockative/src/commonMain/kotlin/io/mockative/SuspendResultBuilder.kt
+++ mockative/src/commonMain/kotlin/io/mockative/SuspendResultBuilder.kt
@@ -4,7 +4,7 @@ class SuspendResultBuilder<R>(
     private val mock: Mockable,
     private val expectation: Expectation
 ) {
-    fun invokes(block: suspend (arguments: Array<Any?>) -> R) {
+    infix fun invokes(block: suspend (arguments: Array<Any?>) -> R) {
         mock.addSuspendStub(OpenSuspendStub(expectation, block))
     }
 
@@ -12,11 +12,11 @@ class SuspendResultBuilder<R>(
         mock.addSuspendStub(ClosedSuspendStub(expectation, blocks))
     }
 
-    fun returns(value: R) = invokes { value }
+    infix fun returns(value: R) = invokes { value }
 
     fun returnsMany(vararg values: R) = invokesMany(values.map { value -> { value } })
 
-    fun throws(throwable: Throwable) = invokes { throw throwable }
+    infix fun throws(throwable: Throwable) = invokes { throw throwable }
 
     fun throwsMany(vararg throwables: Throwable) = invokesMany(throwables.map { throwable -> { throw throwable } })
 }
