e483d5c6ab150846d5065694ab10bc0231725e2a
Nicklas Jensen <800751+Nillerr@users.noreply.github.com>
2024-04-29 19:11:15 +0100

    Fixes #73 by removing unnecessary invoke functions

--- mockative/src/commonMain/kotlin/io/mockative/SuspendResultBuilder.kt
+++ mockative/src/commonMain/kotlin/io/mockative/SuspendResultBuilder.kt
@@ -3,7 +3,7 @@ package io.mockative
 class SuspendResultBuilder<R>(
     private val mock: Mockable,
     private val expectation: Expectation
-) : AnySuspendResultBuilder<R> {
+) {
     fun invokes(block: suspend (arguments: Array<Any?>) -> R) {
         mock.addSuspendStub(OpenSuspendStub(expectation, block))
     }
@@ -12,7 +12,13 @@ class SuspendResultBuilder<R>(
         mock.addSuspendStub(ClosedSuspendStub(expectation, blocks))
     }
 
-    override fun invokes(block: suspend () -> R) = invokes { _ -> block() }
+    fun returns(value: R) = invokes { value }
 
-    override fun invokesMany(vararg blocks: suspend () -> R) = invokesMany(blocks.map { block -> { _ -> block() } })
+    fun returnsMany(vararg values: R) = invokesMany(values.map { value -> { value } })
+
+    fun throws(throwable: Throwable) = invokes { throw throwable }
+
+    fun throwsMany(vararg throwables: Throwable) = invokesMany(throwables.map { throwable -> { throw throwable } })
 }
+
+fun SuspendResultBuilder<Unit>.doesNothing() = invokes { }
