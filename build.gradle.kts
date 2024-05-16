859bcb509969850be1095adb451907ea6752c0cd
Nicklas Jensen <800751+Nillerr@users.noreply.github.com>
2024-05-16 10:26:44 -0400

    Update Android Gradle Plugin to 8.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
 
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0-RC3")
-        classpath("com.android.tools.build:gradle:8.0.2")
+        classpath("com.android.tools.build:gradle:8.4.0")
     }
 }
 
