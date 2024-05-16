2a88230390ea9625ef90a830d326ee1064a51103
Nicklas Jensen <800751+Nillerr@users.noreply.github.com>
2024-05-16 11:07:38 -0400

    Update KotlinPoet to 1.16.0

--- mockative-processor/build.gradle.kts
+++ mockative-processor/build.gradle.kts
@@ -18,8 +18,8 @@ kotlin {
                 implementation("com.google.devtools.ksp:symbol-processing-api:2.0.0-RC3-1.0.20")
 
                 // KotlinPoet
-                implementation("com.squareup:kotlinpoet:1.14.2")
-                implementation("com.squareup:kotlinpoet-ksp:1.14.2")
+                implementation("com.squareup:kotlinpoet:1.16.0")
+                implementation("com.squareup:kotlinpoet-ksp:1.16.0")
             }
 
             kotlin.srcDir("src/main/kotlin")
