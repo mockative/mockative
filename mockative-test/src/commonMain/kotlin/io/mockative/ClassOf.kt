f2f5852a4e602c80aa5a482fb9c85cb3044efcc4
Nicklas Jensen <800751+Nillerr@users.noreply.github.com>
2024-10-31 23:15:12 -0400

    Fix implementation of AtomicReference

--- mockative-test/src/commonMain/kotlin/io/mockative/ClassOf.kt
+++ mockative-test/src/commonMain/kotlin/io/mockative/ClassOf.kt
@@ -9,3 +9,12 @@ import kotlin.reflect.KClass
  * This function serves as a convenient way to avoid having to add a type cast when working with generics.
  */
 inline fun <reified T : Any> of(): KClass<T> = T::class
+
+/**
+ * Returns an instance of [KClass] while retaining the generic type information when passed to a
+ * function accepting an instance of [KClass], allowing that function to use the type arguments.
+ *
+ * This function serves as a convenient way to avoid having to add a type cast when working with generics.
+ */
+@Deprecated(message = "Replaced by `of`", replaceWith = ReplaceWith("of<T>()"))
+inline fun <reified T : Any> classOf(): KClass<T> = T::class
