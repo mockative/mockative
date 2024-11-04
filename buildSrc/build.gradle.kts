845a5aad25586ec2a91b2b3de05f5a4799ff9719
Nicklas Jensen <800751+Nillerr@users.noreply.github.com>
2024-11-04 15:44:23 -0500

    Publish to Maven Central

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,4 +10,5 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.21")
     implementation("com.android.tools.build:gradle:8.0.2")
+    implementation("com.vanniktech:gradle-maven-publish-plugin:0.30.0")
 }
