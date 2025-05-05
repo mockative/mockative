5ba18c2e85445404f4aec2c54c789bb565fd25f2
Michał Saleniuk <saleniuk@gmail.com>
2025-05-05 18:47:03 +0200

    fix: duplicates while merging dex archives

--- mockative/src/commonMain/kotlin/io/mockative/InvocationResult.kt
+++ mockative/src/commonMain/kotlin/io/mockative/InvocationResult.kt
@@ -1,13 +1,13 @@
 package io.mockative
 
 sealed class InvocationResult {
-    class Exception(val exception: Throwable) : InvocationResult() {
+    class Exception(val exception: Throwable) : io.mockative.InvocationResult() {
         override fun toString(): String {
             return "throw ${exception.getClassName()}() $exception"
         }
     }
 
-    class Return(val value: Any?) : InvocationResult() {
+    class Return(val value: Any?) : io.mockative.InvocationResult() {
         override fun toString(): String {
             return "$value"
         }
