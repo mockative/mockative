7fffd56fa6e7d4b6b72161ab343a094e903bbb3a
Nicklas Jensen <800751+Nillerr@users.noreply.github.com>
2026-01-08 22:11:57 +0100

    Upgrade dependencies and plugins to support Kotlin 2.3.0, KSP 2.3.4, and AGP 8.12.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,8 @@ buildscript {
     }
 
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.21")
-        classpath("com.android.tools.build:gradle:8.5.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.0")
+        classpath("com.android.tools.build:gradle:8.12.3")
     }
 }
 
