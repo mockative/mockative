2312f5cb012f3c19d3f42521b961dda1d4f28a2b
Nicklas Jensen <800751+Nillerr@users.noreply.github.com>
2026-02-27 22:00:15 -0500

    Release v3.2.1

--- mockative-plugin/src/main/kotlin/io/mockative/MockativePlugin.kt
+++ mockative-plugin/src/main/kotlin/io/mockative/MockativePlugin.kt
@@ -9,7 +9,7 @@ import org.jetbrains.kotlin.gradle.dsl.kotlinExtension
 import java.io.File
 
 abstract class MockativePlugin : Plugin<Project> {
-    private val version = "3.1.0"
+    private val version = "3.2.1"
 
     override fun apply(project: Project) {
         project.pluginManager.apply("com.google.devtools.ksp")
