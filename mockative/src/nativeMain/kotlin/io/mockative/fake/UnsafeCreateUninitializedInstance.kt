6e966ded900775c50cbe121e959e9fa56111a376
Nicklas Jensen <800751+Nillerr@users.noreply.github.com>
2026-05-13 11:52:56 -0400

    Fix: update suppression and imports for internal Kotlin/Native API usage

--- mockative/src/nativeMain/kotlin/io/mockative/fake/UnsafeCreateUninitializedInstance.kt
+++ mockative/src/nativeMain/kotlin/io/mockative/fake/UnsafeCreateUninitializedInstance.kt
@@ -1,19 +1,21 @@
-@file:Suppress("INVISIBLE_MEMBER", "INVISIBLE_REFERENCE")
+@file:Suppress("INVISIBLE_MEMBER", "INVISIBLE_REFERENCE", "ERROR_SUPPRESSION")
 
 package io.mockative.fake
 
 import io.mockative.InternalMockativeApi
+import kotlin.native.internal.InternalForKotlinNative
+import kotlin.native.internal.createUninitializedInstance
 
 /**
  * Creates an uninitialized instance of [T] on Kotlin/Native using
- * [kotlin.native.internal.createUninitializedInstance].
+ * [createUninitializedInstance].
  *
  * **Do not use this function directly.** It exists solely for Mockative's KSP-generated
  * `makeValueOf` implementation and bypasses all constructor logic, leaving the object in
  * an undefined state. Any use outside of generated matcher code is unsupported and unsafe.
  */
 @InternalMockativeApi
-@OptIn(kotlin.native.internal.InternalForKotlinNative::class)
+@OptIn(InternalForKotlinNative::class)
 inline fun <reified T> unsafeCreateUninitializedInstance(): T {
-    return kotlin.native.internal.createUninitializedInstance()
+    return createUninitializedInstance()
 }
